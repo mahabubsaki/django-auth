55b516ae7aa92252f06c74bf37c620d3916b6368
mahabubsaki <md929820@gmail.com>
2023-09-03 08:25:04 +0600

    learned cookie and session

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from main.views import signup,login,profile,logout,pass_change,pass_change2
+from main.views import signup,login,profile,logout,pass_change,pass_change2,cookie_demo,get_cookie,set_session,get_session
 
 urlpatterns=[
     path('',signup,name='signup'),
@@ -8,4 +8,8 @@ urlpatterns=[
     path('logout/',logout,name='logout'),
     path('passchange/',pass_change,name='passchange'),
     path('passchange2/',pass_change2,name='passchange2'),
+    # path('cookiew/',cookie_demo,name='cookiew'),
+    # path('cookiew/',get_cookie,name='cookiew'),
+    # path('cookiew/',set_session,name='cookiew'),
+    path('cookiew/',get_session,name='cookiew'),
 ]
--- main/views.py
+++ main/views.py
@@ -3,8 +3,8 @@ from main.forms import RegisterForm,UserChange
 from django.contrib import messages
 from django.contrib.auth.forms import AuthenticationForm,PasswordChangeForm,SetPasswordForm
 from django.contrib.auth import authenticate,login as user_login,logout as user_logout,update_session_auth_hash
-
-
+from datetime import datetime,timedelta
+from django.http import HttpResponse
 
 # Create your views here.
 def signup(request):
@@ -104,3 +104,56 @@ def change_user(request):
     else:
         form =UserChange()
     return render(request,"profile.html",{'form':form})
+
+def cookie_demo(request):
+    response = render(request,"cookie.html")
+    response.set_cookie('name','rahim',max_age=3600,expires=datetime.utcnow()+timedelta(days=7))
+    return response
+def get_cookie(request):
+    
+    name = request.COOKIES.get('name')
+    print(request.COOKIES)
+    return render(request,'cookie.html',{'name':name})
+
+
+def delete_cookie(request):
+    response = render(request,"cookie.html")
+    response.delete_cookie('name')
+    return response
+
+def set_session(request):
+
+    data = {
+        'name':'rahim',
+        'age':23,
+        'language':'bangla'
+    }
+    # get session expiration age
+    session_age = request.session.get_session_cookie_age()
+    #get session expiray data
+    expiray_date = request.session.get_session_cookie_age()
+    
+    request.session.update(data)
+    return render(request,'cookie.html')
+
+def get_session(request):
+    if 'name' in request.session:
+
+        name= request.session.get('name')
+        age= request.session.get('age')
+        language= request.session.get('language')
+        data = {'name':name,'age':age,'language':language}
+        # update the session expiry age
+        request.session.modified = True
+        return render(request,'cookie.html',{'data':data})
+    else:
+        return HttpResponse("Your Session has been expired")
+
+
+def delete_session(request):
+     # for deleting particular data
+    del request.session['name']
+    #  for deleting all data
+    request.session.flush()
+    request.session.clear_expired() #
+    return render(request,'cookie.html')
